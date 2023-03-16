49bdb7dbac7b11362edeea754c1ff5c0ed6e111d
Iman Moghaddas <imanmoghaddas@yahoo.com>
2023-03-16 14:46:13 -0400

    Fixed the getRole in the different classes so they all match. Also added Readme file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # profileGenerator
+
+## Description
 A Node.js command-line application that takes in information about employees on a software engineering team and generates an HTML webpage that displays summaries for each person.
+
+## Installation
+N/A
+
+## Usage
+![Alt text](/assets/screen-recording.mov)
+
+## Credits
+https://github.com/imanmogh/profileGenerator
+
+## License
+M/A
--- index.js
+++ index.js
@@ -1,37 +1,22 @@
-/*GIVEN a command-line application that accepts user input
-WHEN I am prompted for my team members and their information
-THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-WHEN I click on an email address in the HTML
-THEN my default email program opens and populates the TO field of the email with the address
-WHEN I click on the GitHub username
-THEN that GitHub profile opens in a new tab
-WHEN I start the application
-THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-WHEN I enter the team manager’s name, employee ID, email address, and office number
-THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-WHEN I select the engineer option
-THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-WHEN I select the intern option
-THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-WHEN I decide to finish building my team
-THEN I exit the application, and the HTML is generated */
 
-const employee = require(`./lib/Employee.js`)
-const engineer = require(`./lib/Engineer.js`)
-const intern = require(`./lib/Intern.js`)
-const manager = require(`./lib/Manager.js`)
-
-
+const Employee = require(`./lib/Employee.js`)
+const Engineer = require(`./lib/Engineer.js`)
+const Intern = require(`./lib/Intern.js`)
+const Manager = require(`./lib/Manager.js`)
+const generateTeam = require("./dist/template.js")
 const inquirer = require('inquirer');
 const fs = require('fs');
+const path = require("path");
 const { type } = require('os');
-const Manager = require('./lib/Manager.js');
+const dist_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(dist_DIR, "team.html");
 
 
 const teamMemberArray = [];
 
+function runApp() {
 
-function team() {
+  function team() {
   inquirer
   .prompt([
     {
@@ -39,20 +24,22 @@ function team() {
       message: 'What type of team member do you want to add?',
       choices: ['Manager', "Intern", 'Engineer', 'No more team members needed'],
       name: 'teamMember',
-    },
+    }
   ])
-  .then(function (answer){
-    if (answer.teamMember === 'Manager') {
-      return addManager;
-    }
-    else if (answer.teamMember === 'Intern') {
-      return addIntern;
-    }
-    else if (answer.teamMember === 'Engineer') {
-      return addEngineer;
-    }
-    else {
-      return "All team members are added. Thank you."
+  .then(function (answer) {
+    switch(answer.teamMember) {
+      case "Manager":
+        addManager();
+        break;
+      case "Engineer":
+        addEngineer();
+        break;
+      case "Intern":
+        addIntern();
+        break;
+
+      default:
+        htmlBuilder();
     }
   })
 }
@@ -91,8 +78,6 @@ function team() {
   })
 }
 
-
-
   function addEngineer() {
     inquirer
     .prompt ([
@@ -120,7 +105,7 @@ function team() {
       }
   ])
   .then(answer => {
-    var manager = new Manager(answer.engineersId, answer.engineersName, answer.engineersEmail, answer.github);
+    var manager = new Engineer(answer.engineersId, answer.engineersName, answer.engineersEmail, answer.github);
     teamMemberArray.push(manager);
     team();
   })
@@ -153,9 +138,23 @@ function team() {
       }
   ])
   .then(answer => {
-    var manager = new Manager(answer.internId, answer.internsName, answer.intenEmail, answer.school);
+    var manager = new Intern(answer.internId, answer.internsName, answer.intenEmail, answer.school);
     teamMemberArray.push(manager);
     team();
   })
-  }
+}
 
+
+
+  function htmlBuilder () {
+    console.log("Your team has been created!")
+
+    fs.writeFileSync('./dist/index.html', generateTeam(teamMemberArray), "UTF-8")
+
+}
+
+team();
+
+}
+
+runApp();
